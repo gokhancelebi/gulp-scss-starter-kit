d09221b7f6cf480112a42cd4793c167182aec0b5
Gökhan Çelebi <dinatopya03@gmail.com>
2021-09-14 10:41:21 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -22,8 +22,5 @@ gulp.task('scss-concat', function () {
 
 gulp.task('watch', function () {
     gulp.watch('assets/scss/parts/*.scss', gulp.series('scss-concat'));
-    gulp.watch('assets/scss/main.scss', gulp.series('scss-concat'));
-    gulp.watch('assets/scss/mobile-md.scss', gulp.series('scss-concat'));
-    gulp.watch('assets/scss/mobile-sm.scss', gulp.series('scss-concat'));
-    gulp.watch('assets/scss/mobile-xs.scss', gulp.series('scss-concat'));
+    gulp.watch('assets/scss/*.scss', gulp.series('scss-concat'));
 });
