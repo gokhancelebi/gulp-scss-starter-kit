3313ed7413f6371536405fbc35d55f072019222a
Gökhan Çelebi <dinatopya03@gmail.com>
2024-01-28 10:04:47 +0300

    yedek

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,24 @@
-// npm install gulp gulp-sass node-sass gulp-clean-css gulp-concat
+/**
+ * npm install --global gulp-cli
+ * npm install gulp gulp-sass sass gulp-clean-css --save-dev
+ *
+ * After all, run "gulp" command on cmd
+ *
+ */
+const {src, dest, watch, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const gulpCleanCss = require('gulp-clean-css');
 
-var gulp = require('gulp'),
-    sass = require('gulp-sass')(require("node-sass")),
-    cleanCSS = require('gulp-clean-css'),
-    concat = require('gulp-concat');
-
-gulp.task('scss-concat', function () {
-    return gulp.src([
-        'assets/scss/reset.scss',
-        'assets/scss/parts/*.scss',
-        'assets/scss/main.scss',
-        'assets/scss/mobile-md.scss',
-        'assets/scss/mobile-sm.scss',
-        'assets/scss/mobile-xs.scss'
-    ])
+function buildStyles() {
+    // place code for your default task here
+    return src('./assets/scss/app.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(concat('bundle.css'))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('assets/css'));
-});
+        .pipe(gulpCleanCss({compatibility: 'ie8'}))
+        .pipe(dest('./assets/build'));
+}
 
-gulp.task('watch', function () {
-    gulp.watch('assets/scss/parts/*.scss', gulp.series('scss-concat'));
-    gulp.watch('assets/scss/*.scss', gulp.series('scss-concat'));
-});
+function watchStyles() {
+    watch('./assets/scss/**/*.scss', buildStyles);
+}
+
+exports.default = series(buildStyles, watchStyles);
--- readme.md
+++
@@ -1,8 +0,0 @@
-Use this command to prepare gulp for usage
-
-npm install gulp gulp-sass node-sass gulp-clean-css gulp-concat
-
-
-and then to watch and auto prepare css use this command
-
-gulp watch
